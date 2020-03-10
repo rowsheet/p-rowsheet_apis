eca9b7e054a29fb9d147661ca318802ed6fdeb7f
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2020-03-09 22:48:25 -0600

    Making default url config to old landing page if no DEPLOYMENT_MODE set.

--- rideshare/urls.py
+++ rideshare/urls.py
@@ -3,13 +3,10 @@ from django.urls import path
 from rideshare import views
 from django.conf import settings
 
-"""
-Since Homobiles has the "old" active production pages, set these as the
-active url route if no other "DEPLOYMENT_MODE" is set.
-For development, add something other than "PRODUCTION" for the "DEPLOYMENT_MODE"
-in the .env file.
-"""
 if settings.DEPLOYMENT_MODE == "PRODUCTION":
+    """
+    This is the old landing page.
+    """
     urlpatterns = [
         # Site pages.
         path("", views.index),
@@ -17,62 +14,13 @@ if settings.DEPLOYMENT_MODE == "PRODUCTION":
         path("for-riders/", views.for_riders),
         path("why-homobiles/", views.why_homobiles),
         path("signup/", views.signup),
-    ]
-if settings.DEPLOYMENT_MODE == "STAGING":
-    urlpatterns = [
-        # Site pages.
-        path("", views.index),
-        path("for-drivers/", views.for_drivers),
-        path("for-riders/", views.for_riders),
-        path("why-homobiles/", views.why_homobiles),
-        path("signup/", views.signup),
-        # DEMO
-        path("demo_google_maps/", views.demo_google_maps),
-        path("geocode/", views.geocode),
-        # On-boarding pages.
-        # path("get_started/", views.get_started),
-        path("", views.get_started),
-        path("phone_verification/", views.phone_verification),
-        path("code_verification/", views.code_verification),
-        # Main pages.
-        path("main_screen/", views.main_screen),
-        path("set_location/", views.set_location),
-        path("account/", views.account),
-        path("profile/", views.profile),
-        # Sidebar pages
-        path("past_rides/", views.past_rides),
-        path("donation_station/", views.donation_station),
-        path("settings/", views._settings),
-        path("driver/", views.driver),
-        path("about/", views.about),
-        path("help/", views.help),
-        # Account pages.
-        path("payment_methods/", views.payment_methods),
-        path("email_address/", views.email_address),
-        path("phone_number/", views.phone_number),
-        # Settings pages.
-        path("services/", views.services),
-        path("location/", views.location),
-        path("notifications/", views.notifications),
-        path("emergency/", views.emergency),
-        path("trusted_contacts/", views.trusted_contacts),
-        path("delete_account/", views.delete_account),
-        # Help pages.
-        path("report_a_recent_ride/", views.report_a_recent_ride),
-        path("report_a_lost_item/", views.report_a_lost_item),
-        path("how_ride_payment_works/", views.how_ride_payment_works),
-        path("free_rides/", views.free_rides),
-        path("saftey/", views.saftey),
-        path("policies/", views.policies),
-        path("legal/", views.legal),
     ]
 else:
+    """
+    This is the new fancy app.
+    """
     urlpatterns = [
-        # DEMO
-        path("demo_google_maps/", views.demo_google_maps),
-        path("geocode/", views.geocode),
         # On-boarding pages.
-        # path("get_started/", views.get_started),
         path("", views.get_started),
         path("phone_verification/", views.phone_verification),
         path("code_verification/", views.code_verification),
@@ -107,4 +55,8 @@ else:
         path("saftey/", views.saftey),
         path("policies/", views.policies),
         path("legal/", views.legal),
+        #-----------TEMP-----------
+        # DEMO
+        path("demo_google_maps/", views.demo_google_maps),
+        path("geocode/", views.geocode),
     ]
