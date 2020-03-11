9a828c4918a870f7ff2da431ee3d4a289bae4abf
Koltyn Palmer <k@alphaomega23.com>
2020-03-11 01:36:33 -0600

    adds team phone# env variables for prod/dev

--- rideshare/views.py
+++ rideshare/views.py
@@ -28,8 +28,10 @@ def send_text_message(body):
     account_sid = "AC24fc9ac27dee145f04d855b99b666ab8"
     auth_token = "08da7fc65a1b8163f17aa324ddef479d"
     client = Client(account_sid, auth_token)
-    num=['+14155745023','+15404540846', '+14158672671', '+16464138190', '+17203643760']
-    # num = ['+15404540846', '+17203643760']  # DEV ONLY
+    if settings.DEPLOYMENT_MODE == "PRODUCTION":
+        num=['+14155745023','+15404540846', '+14158672671', '+16464138190', '+17203643760']
+    else:
+        num = ['+15404540846', '+17203643760'];  # DEV ONLY
     for i in range(0, len(num)):
         message = client.messages.create(
             num[i],
